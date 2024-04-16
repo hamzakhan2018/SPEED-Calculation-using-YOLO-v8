411015542b3dd128e4b3772838f8f857b1a0997d
Hamza Khan <121418345+hamzakhan2018@users.noreply.github.com>
2024-04-16 12:20:50 +0500

    speed.py

--- speed.py
+++ speed.py
@@ -5,7 +5,7 @@ import cv2
 model = YOLO("yolov8n.pt")
 names = model.model.names
 
-cap = cv2.VideoCapture("D:/HAMZA_WORK/vision_eye/road.mp4")
+cap = cv2.VideoCapture("path/road.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
